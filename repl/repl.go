33893489d5a1a7026411096d248391a199c256de
GrGLeo <graziani.gothe.leo@gmail.com>
2025-03-08 09:29:39 +0100

    evaluating integer

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/GrGLeo/monkey/evaluator"
 	"github.com/GrGLeo/monkey/lexer"
 	"github.com/GrGLeo/monkey/parser"
 )
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+    evaluated := evaluator.Eval(program)
+    if evaluated != nil {
+      io.WriteString(out, evaluated.Inspect())
+      io.WriteString(out, "\n")
+    }
 	}
 }
 
